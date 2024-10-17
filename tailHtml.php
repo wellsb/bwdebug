d69a2d2237957f34e38da28d27e1dec2b34787ad
Brendan WellsDevDesk01 <wells862@hotmail.com>
2024-10-17 23:46:44 +0100

    Added Colour support, config options

--- tailHtml.php
+++ tailHtml.php
@@ -7,69 +7,135 @@ TODO
 */
 
 /**
- * Debug function to dump a variable to a log file.
+ * Debug function to dump variables to a log file.
+ * 
+ * output "some string" to default file     = bwdebug("some string");
+ * output labelled array to default file    = bwdebug(["fruit", $fruit]);
+ * output "a string" to file 2 with header  = bwdebug("a string", 2, 1);
+ * labelled array to file 2                 = bwdebug(["fruit", $fruit], 2);
+ * output "a string" to File 1 with header  = bwdebug("a string", 1, 1);
+ * output int(1) to default file            = bwdebug(1);
  *
  * @param mixed $Capture The variable to dump.
  * @param int $File The file number to write to (1 or 2). Defaults to 1.
- * @param bool $first Whether this is the first entry in the log file. Defaults to false.
+ * @param bool $first Whether this is the first entry in the log file (output debug header). Defaults to false.
  */
 function bwdebug($Capture, $File = 1, $first = false) {
 
-    // -------------
-    $tabItOut = 1;
-    $genRandNumberForStartMarker = 1;
-    // -------------
-var_dump($Capture);
-    $output = "\n-";
+    // -Config------
+        // Tab out method headers
+        $tabItOut = 1;
 
+        // Output method var_dump || print_r
+        $outputMethod = 'var_dump';
+
+        // Start each capture with a random number
+        $genRandNumberForStartMarker = 1;
+
+        // Blank lines before output
+        $blankLinesBetweenOutputs = 1;
+
+        // Try to output coloured outputs
+        $colouredOutput = true;
+            $colouredDebugHeaders = true;
+            $colourMethodHeaders = true;
+
+        // Debug to raw $Capture to StdOut
+        $debugToStdOut = true;
+    // -------------
+
+    // Debug to console
+    if ($debugToStdOut) {
+        var_dump($Capture);
+    }
+
+    $output = "";
+
+    // If set insert blank lines
+    if ($blankLinesBetweenOutputs > 0) {
+        for ($i = 0; $i <= $blankLinesBetweenOutputs; $i++) {
+            $output .= "\n";
+        }
+    }
+
+    // Work out what file to send this output
     if ($File == 1) {
         $filename = 'output.log';
     } else {
         $filename = 'output2.log';
     }
 
-    if ($first) {
-        if ($genRandNumberForStartMarker) {
-            $output .= str_pad(rand(0, 99), 2, '0', STR_PAD_LEFT);
-        }
-        $output .= "-----".date('H:i:s')."------------\n";
-    } else {
+
+    if (!$first) {
+        // Is not first (could be method header or normal dump)
         if (is_string($Capture) && strpos($Capture, '**') === 0) {
-            // is method header (starts with **)
+            // It's a method header (starts with **)
             if ($tabItOut) {
                 $capStrs = explode("#", $Capture);
                 foreach ($capStrs as $capStr)
                 {
-                    $output .= $capStr."\t";
+                    if ($colouredOutput && $colourMethodHeaders) {
+                        $output .= "\033[33m".$capStr."\t \033[0m";
+                    } else {
+                        $output .= $capStr."\t";
+                    }
                 }
 
-                // trim the ** used to detect method headers
+                // Trim the ** used to detect method headers
                 $output = str_replace('**', '', $output);
 
-                // trim the last tab
+                // Trim the last tab
                 $output = rtrim($output, "\t")."\n";
+            } else {
+                // None tabbed method header
+                $output = $Capture;
             }
         } else {
+            // Default output
             ob_start();
-            var_dump($Capture);
+            if ($outputMethod == 'var_dump')
+            {
+                var_dump($Capture);
+            } else if ($outputMethod == 'print_r')
+            {
+                print_r($Capture);
+            }
             $output = ob_get_clean();
         }
-    }
+    } else {
+        // Is first - print debug header
+        if ($colouredOutput && $colouredDebugHeaders) {
+            $output .= "\033[32m";
+        }
 
+        if ($genRandNumberForStartMarker) {
+            $output .= "-".str_pad(rand(0, 99), 2, '0', STR_PAD_LEFT)."-----".date('H:i:s')."------------\n";
+        }
+
+        if ($colouredOutput && $colouredDebugHeaders) {
+            $output .= "\033[0m";
+        }
+        $output .= $Capture;
+
+
+
+
+    }
     file_put_contents($filename, $output, FILE_APPEND);
 }
 
-bwdebug("stringy", 1, 1);
-bwdebug("stringy");
-bwdebug(1);
-
+// Test arrays
 $birds = ['blue', 'tit', 'pigeon'];
 $fruit = ['apple', 'bannana', 'pear'];
 $cars = ['fird', 'pergeot', 'vauxhall'];
 
+
+bwdebug("a string", 1, 1);
+//bwdebug("another string");
+//bwdebug(1);
 //bwdebug("", 1, 1);
 //bwdebug($birds, 1, 1);
-//bwdebug(["fruit", $fruit]);
+//bwdebug(["fruit", $fruit], 2);
 //bwdebug($cars, 2);
 //bwdebug($birds, 2, true);
 //bwdebug (dirname(__FILE__)."#".basename(__FILE__)."#".__FUNCTION__);
@@ -82,6 +148,7 @@ genRandHtml(1);
 //     sleep(2);
 // }
 
+// test function
 function genRandHtml($num_lines) {
     bwdebug("**".dirname(__FILE__)."#".basename(__FILE__)."#".__CLASS__."#->".__FUNCTION__."():".__LINE__);
     //bwdebug("sakldjh");
